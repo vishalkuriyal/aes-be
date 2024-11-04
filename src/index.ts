09310b47bb705fd834089b6a389fbebd2e57424a
Ayush Semwal <ayushsemwal648@gmail.com>
2024-11-04 08:09:31 -0500

    update origin

--- src/index.ts
+++ src/index.ts
@@ -92,11 +92,22 @@ if (process.env.PRODUCTION === "false") {
 }
 
 
-// const allowedOrigins = ['http://localhost:3000', "https://aes-website.vercel.app", "aes-website.vercel.app", "https://aes-website.vercel.app/", "https://aesrecruitment.com", "https://www.aesrecruitment.com", "http://aesrecruitment.com"];
+const allowedOrigins = ['http://localhost:3000', "https://aes-website.vercel.app", "aes-website.vercel.app", "https://aes-website.vercel.app/", "https://aesrecruitment.com", "https://www.aesrecruitment.com", "http://aesrecruitment.com"];
 app.use(
   cors({
     credentials: true,
-    origin: ['*'],
+    origin: function(origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
   })
 );
 
