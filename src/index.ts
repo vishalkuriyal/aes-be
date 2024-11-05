34a3e95d06b88f3cd169eebc9682e212656f4999
Piyush Duggal <duggalpiyush0@gmail.com>
2024-11-05 11:06:41 +0530

    remove cors

--- src/index.ts
+++ src/index.ts
@@ -88,35 +88,10 @@ if (process.env.PRODUCTION === 'false') {
     })
   );
 }
-
-const allowedOrigins = [
-  'aesrecruitment.com',
-  'www.aesrecruitment.com',
-  'http://localhost:3000',
-  'https://aes-website.vercel.app',
-  'aes-website.vercel.app',
-  'https://aes-website.vercel.app/',
-  'https://aesrecruitment.com',
-  'https://www.aesrecruitment.com',
-  'http://aesrecruitment.com',
-  'http://aesrecruitment.com',
-];
 app.use(
   cors({
     credentials: true,
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          'The CORS policy for this site does not ' +
-          'allow access from the specified Origin.' +
-          origin;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: '*',
   })
 );
 
