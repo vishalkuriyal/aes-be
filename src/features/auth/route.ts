e4c657a4797e9df25bc583fc0e124a633262eabc
Piyush Duggal <duggalpiyush0@gmail.com>
2024-11-05 12:14:16 +0530

    removed register route

--- src/features/auth/route.ts
+++ src/features/auth/route.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from 'express';
-import { isAuth, loginUser, logoutUser, registerUser } from './controller';
+import { isAuth, loginUser, logoutUser } from './controller';
 
 export const authRouter = express.Router();
-authRouter.post('/register', registerUser);
+// authRouter.post('/register', registerUser);
 authRouter.post('/login', loginUser);
 authRouter.post('/logout', logoutUser);
 authRouter.get('/me', isAuth, (req: Request, res: Response) => {
