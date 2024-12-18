bc62e9d9492b886d28bb66cf8157cd1b69d2bafa
Piyush Duggal <duggalpiyush0@gmail.com>
2024-12-17 22:06:17 -0500

    update image

--- src/utils/drive.ts
+++ src/utils/drive.ts
@@ -1,21 +1,22 @@
-
 import fs from 'fs';
 import { google } from 'googleapis';
-import dotenv from 'dotenv'
-dotenv.config()
-
+import dotenv from 'dotenv';
+dotenv.config();
 
 const CLIENT_ID = process.env.CLIENT_ID as string;
 const CLIENT_SECRET = process.env.CLIENT_SECRET as string;
 const REDIRECT_URI = process.env.REDIRECT_URI as string;
 
-const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
 const drive = google.drive({ version: 'v3', auth: oauth2Client });
 
 oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 export const uploadFileToDrive = async (filePath: string, fileName: string) => {
-
   const fileMetadata = {
     name: fileName,
   };
@@ -41,12 +42,10 @@ export const uploadFileToDrive = async (filePath: string, fileName: string) => {
       type: 'anyone',
     },
   });
-
-  const fileUrl = `https://drive.google.com/thumbnail?id=${fileId}`;
+  const fileUrl = `https://lh3.googleusercontent.com/d/${fileId}`;
   return fileUrl;
 };
 
 export const deleteFileFromDrive = async (fileId: string) => {
   await drive.files.delete({ fileId });
 };
-
